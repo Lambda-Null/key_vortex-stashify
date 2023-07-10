4ca1b5ee8751f63a15302bd3280a0e24a6dac576
Lambda Null <lambda.null.42@gmail.com>
2023-07-10 17:47:50 -0400

    Initial commit

---
+++ lib/key_vortex/stashify/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module KeyVortex
+  module Stashify
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/key_vortex/stashify_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe KeyVortex::Stashify do
+  it "has a version number" do
+    expect(KeyVortex::Stashify::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
